3effb1b80297c5cc0feb5c3302dcbbf7fad4c4ec
Gabriel <ilie.cgabriel@gmail.com>
2022-06-15 23:15:37 +0300

    small rename

--- web-app/src/screens/main/form.tsx
+++ web-app/src/screens/main/form.tsx
@@ -2,11 +2,11 @@ import { FunctionComponent } from 'react'
 import { useTimer } from './useTimer'
 
 export const Form: FunctionComponent = () => {
-    const { start, time } = useTimer(5);
+    const { start: startTimer, time } = useTimer(5);
     return (
         <>
             <div>time {time}</div>
-            <button onClick={start}>Start ticking</button>
+            <button onClick={startTimer}>Start ticking</button>
         </>
     )
 }
