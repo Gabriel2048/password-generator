8cf14004ed68b3a25deb5d27e7fd7c7646facb81
Gabriel <ilie.cgabriel@gmail.com>
2022-06-18 16:16:38 +0300

    coding style fix

--- web-api/PasswordGenerator.Api/Controllers/PasswordController.cs
+++ web-api/PasswordGenerator.Api/Controllers/PasswordController.cs
@@ -2,25 +2,24 @@
 using Microsoft.AspNetCore.Mvc;
 using PasswordGenerator.Application.Password.Commands.CreatePassword;
 
-namespace PasswordGenerator.Api.Controllers
+namespace PasswordGenerator.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PasswordController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PasswordController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public PasswordController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public PasswordController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreatePasswordForUser([FromBody] string userId)
-        {
-            var command = new CreatePasswordCommand(userId);
-            var response = await _mediator.Send(command);
-            return Ok(response);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreatePasswordForUser([FromBody] string userId)
+    {
+        var command = new CreatePasswordCommand(userId);
+        var response = await _mediator.Send(command);
+        return Ok(response);
     }
 }
